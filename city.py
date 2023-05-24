5c6c10235f53fb024e5133db294fa8659fc3f153
bielfortunyc <133250458+bielfortunyc@users.noreply.github.com>
2023-05-24 17:21:58 +0200

    Update city.py

--- city.py
+++ city.py
@@ -1,22 +1,33 @@
 import networkx
 from typing import TypeAlias
+import osmnx as ox
 
 CityGraph : TypeAlias = networkx.Graph
-
-def get_osmnx_graph() -> OsmnxGraph: ... 
-
 OsmnxGraph : TypeAlias = networkx.MultiDiGraph
-
-def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None: ...
-    # guarda el graf g al fitxer filename
+Coord : TypeAlias = tuple[float, float]   # (latitude, longitude)
+def get_osmnx_graph() -> OsmnxGraph:
+    ciutat = "Barcelona, Spain"
+    graf = ox.graph_from_place(ciutat, network_type="all")
     
-def load_osmnx_graph(filename: str) -> OsmnxGraph: ...
+    return graf
+
+def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None:
+    # guarda el graf g al fitxer filename
+    output_folder = "Desktop/PRACTICA2"
+    
+
+    ox.save_graph_shapefile(g, output_folder, filename)
+    
+def load_osmnx_graph(filename: str) -> OsmnxGraph: 
     # retorna el graf guardat al fitxer filename
+    ruta_document = 'Desktop/PRACTICA2'
+    graf = ox.load_graphml(ruta_document, filename)
+    return graf
 
 def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph: ...
     # retorna un graf fusió de g1 i g2
     
-Coord : TypeAlias = tuple[float, float]   # (latitude, longitude)
+
 
 def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path: ...
 
@@ -24,6 +35,8 @@ def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path: .
 def show(g: CityGraph) -> None: ...
     # mostra g de forma interactiva en una finestra
 def plot(g: CityGraph, filename: str) -> None: ...
-    # desa g com una imatge amb el mapa de la cuitat de fons en l'arxiu filename
+   # desa g com una imatge amb el mapa de la cuitat de fons en l'arxiu filename
 def plot_path(g: CityGraph, p: Path, filename: str, ...) -> None: ...
     # mostra el camí p en l'arxiu filename
+    
+save_osmnx_graph(get_osmnx_graph(), 'graf_barcelona')
