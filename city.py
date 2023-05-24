a08485e76b91c2315c8be94c240d27b7725ce3c8
bielfortunyc <133250458+bielfortunyc@users.noreply.github.com>
2023-05-24 18:27:22 +0200

    Update city.py

--- city.py
+++ city.py
@@ -1,15 +1,17 @@
-import networkx
+import networkx as nx
 from typing import TypeAlias
 import osmnx as ox
+from buses import *
 
-CityGraph : TypeAlias = networkx.Graph
-OsmnxGraph : TypeAlias = networkx.MultiDiGraph
+
+CityGraph : TypeAlias = nx.Graph
+OsmnxGraph : TypeAlias = nx.MultiDiGraph
 Coord : TypeAlias = tuple[float, float]   # (latitude, longitude)
 def get_osmnx_graph() -> OsmnxGraph:
     ciutat = "Barcelona, Spain"
     graf = ox.graph_from_place(ciutat, network_type="all")
-    
-    return graf
+    multi_graf = nx.MultiDiGraph(graf)
+    return multi_graf
 
 def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None:
     # guarda el graf g al fitxer filename
@@ -24,11 +26,16 @@ def load_osmnx_graph(filename: str) -> OsmnxGraph:
     graf = ox.load_graphml(ruta_document, filename)
     return graf
 
-def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph: ...
+def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph:
     # retorna un graf fusió de g1 i g2
     
+    g1 = load_osmnx_graph('graf_barcelona')
+    g2 = get_buses_graph()
+    
+    city = nx.Graph()
+    
 
-
+    return city
 def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path: ...
 
 
