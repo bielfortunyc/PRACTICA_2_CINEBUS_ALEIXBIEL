9e948fbbb34d9bffa61587f4e41aef6e4347399d
aleix-albaiges <118050410+aleix-albaiges@users.noreply.github.com>
2023-05-30 16:22:47 +0200

    Update buses.py

--- buses.py
+++ buses.py
@@ -1,121 +1,140 @@
-from typing import TypeAlias
-from dataclasses import dataclass
-import json
 import networkx as nx
-import urllib.request
-import staticmap
-import os
+from typing import TypeAlias, Optional
+import osmnx as ox
+import pickle
 import matplotlib.pyplot as plt
-from PIL import Image
-BusesGraph : TypeAlias = nx.Graph
-Coord: TypeAlias = tuple[float,float]
+import os
+from dataclasses import dataclass
+from provabuses import *
+CityGraph : TypeAlias = nx.Graph
+OsmnxGraph : TypeAlias = nx.MultiDiGraph
 
 @dataclass
-class Parada:
-    id: str
+class Cruilla:
+    id: int
     pos: Coord
-    def __init__(self,id:int,pos: Coord)->None:
+    def __init__(self, id: int, pos:Coord) -> None:
         self.id = id
         self.pos = pos
-        
-class Bus:
-    node_origen: Parada
-    node_desti: Parada
-    distancia: float
+class Carrer:
+    node_origen_id: int
+    node_desti_id: int
+    longitud: float
     info: str
-    def __init__(self, node_origen: Parada, node_desti: Parada, distancia:float, info: str) -> None:
-        self.node_origen = node_origen
-        self.node_desti = node_desti
-        self.distancia = distancia
+    def __init__(self, node_origen_id: int, node_desti_id: int, longitud: float, info:str) -> None:
+        self.node_origen_id = node_origen_id
+        self.node_desti_id = node_desti_id
+        self.longitud = longitud
         self.info = info
-        
-        
-def distance(pos1:tuple[float,float], pos2:tuple[float,float]) -> float:
-    d = (pos1[0] - pos2[0])**2 + \
-            (pos1[1] - pos2[1])**2
-    return d**(1/2)
-       
-def get_buses_graph() -> nx.Graph:
-    # Llegir les dades JSON
-    url = 'https://www.ambmobilitat.cat/OpenData/ObtenirDadesAMB.json'
-    with urllib.request.urlopen(url) as response:
-        data = json.loads(response.read().decode())
 
-    # Crear el graf de busos
-    graph = nx.Graph()
+def get_osmnx_graph() -> OsmnxGraph:
+    filename = 'graf_barcelona'
+    if os.path.exists(filename):
+        return load_osmnx_graph(filename)
+    else:
+        ciutat = "Barcelona, Spain"
+        graf = ox.graph_from_place(ciutat, network_type='walk', simplify=True)
+        multi_graf = nx.MultiDiGraph(graf)
+        save_osmnx_graph(graf,"graf_barcelona")
+        return multi_graf
 
-   # Processar les parades i afegir-les com a nodes amb els seus atributs corresponents
-    for i in range(len(data['ObtenirDadesAMBResult']['Linies']['Linia'])):
-        parades = data['ObtenirDadesAMBResult']['Linies']['Linia'][i]['Parades']['Parada']
-        for parada in parades:
-            if parada['Municipi'] == 'Barcelona':
-                node_id = parada['CodAMB']
+def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None:
+    # guarda el graf g al fitxer filename
+    # Obtenir el directori actual
+    current_dir = os.getcwd()
+
+    # Definir la ruta del fitxer de sortida
+    output_folder = os.path.join(current_dir, filename)
+    with open(output_folder, 'wb') as file:
+        pickle.dump(g, file)
+
     
-                node_pos = (parada['UTM_X'], parada['UTM_Y'])
+def load_osmnx_graph(filename: str) -> OsmnxGraph: 
+    # retorna el graf guardat al fitxer filename
+      # Obtenir el directori actual
+    current_dir = os.getcwd()
 
-                # Afegir el node amb els atributs de nom i coordenades
-                parada = Parada(id=node_id, pos = node_pos)
-                graph.add_node(parada.id, pos=node_pos, node = parada)
+    # Definir la ruta del fitxer
+    ruta_document = os.path.join(current_dir, filename)
 
-    # Processar les arestes i afegir-les com a arestes amb els seus atributs corresponents
-    for linia in data['ObtenirDadesAMBResult']['Linies']['Linia']:
-        parades_linia = linia['Parades']['Parada']
-        
-                
-        
-        for i in range(len(parades_linia) - 1):
-            dic_node_origen = parades_linia[i]
-            dic_node_desti = parades_linia[i+1]
-            if dic_node_origen['Municipi'] == 'Barcelona' and dic_node_desti['Municipi'] == 'Barcelona':
-                node_origen_id = dic_node_origen['CodAMB']
-                node_desti_id = dic_node_desti['CodAMB']
-                node_origen_pos = (dic_node_origen['UTM_X'], dic_node_origen['UTM_Y'])
-                node_desti_pos = (dic_node_desti['UTM_X'],dic_node_desti['UTM_Y'])
-                    
-                node_origen = Parada(id = node_origen_id, pos = node_origen_pos)
-                node_desti = Parada(id = node_desti_id,pos = node_desti_pos)
-                
-            # Afegir l'aresta amb l'atribut de la línia de bus
-            distancia = distance(node_origen.pos,node_desti.pos)
-            edge = Bus(node_origen, node_desti, distancia, 'bus')
-            if node_origen_id != node_desti_id:
-                graph.add_edge(node_origen_id, node_desti_id, aresta = edge, length = distancia)   
+    # Comprovar si el fitxer existeix
+    if not os.path.exists(ruta_document):
+        raise FileNotFoundError(f"El fitxer '{filename}' no existeix en el directori actual.")
+
+    # Llegir el graf des del fitxer
+    with open(ruta_document, 'rb') as file:
+        graf = pickle.load(file)
+    return graf
+
+def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph:
+    # retorna un graf fusió de g1 i g2
     
-    return graph
-
-def show(g: BusesGraph) -> None:
+    city_graph = nx.Graph()
+    for node_id, data in g1.nodes(data=True):
+        cruilla = Cruilla(id=node_id, pos=(data['y'],data['x']))
+        city_graph.add_node(cruilla.id, pos= cruilla.pos, node = cruilla)
     
-    posicions = nx.get_node_attributes(g, 'pos')
-    nx.draw(g, pos=posicions, with_labels=False,font_size=5,node_size= 5)
+    for u, v, data in g1.edges(data=True):
+        carrer = Carrer(node_origen_id = u, node_desti_id = v, longitud= data['length'], info = 'Carrer')
+        if 'geometry' in data:
+            del(data['geometry'])
+        city_graph.add_edge(carrer.node_origen_id,carrer.node_desti_id, aresta = carrer)
+    
+    for node_attrs in g2.nodes.values():
+        parada = Parada(id = node_attrs['node'].id, pos = node_attrs['node'].pos)
+        city_graph.add_node(parada.id, pos = parada.pos, node = parada, info = 'Bus')
+        cruilla_propera = ox.nearest_nodes(g1, parada.pos[0], parada.pos[1])
+        city_graph.add_edge(parada.id,cruilla_propera, aresta = cruilla_propera, info = 'Carrer')
+        #no detecta que parada i cruilla_propera estiguin ja al graf i genera problems 
+        #perquè els torna a afegir sense atributs.
+    
+    
+    for edge_attrs in g2.edges.values():        
+        linia = edge_attrs['aresta']  
+        city_graph.add_edge(linia.node_origen.id, linia.node_desti.id, info = 'Bus')
+    
+        
+        
+    return city_graph
+
+"""
+def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path: 
+    src_node = ox.distance.nearest_nodes(ox_g, src[0], src[1])
+    dst_node = ox.distance.nearest_nodes(ox_g, dst[0], dst[1])
+
+    shortest_path = nx.shortest_path(g, src_node, dst_node)
+    shortest_edges = [(shortest_path[i], shortest_path[i+1]) for i in range(len(shortest_path)-1)]
+    
+    return Path(shortest_path, shortest_edges)
+"""
+def show(g: CityGraph) -> None:
+    # mostra g de forma interactiva en una finestra
+    positions = {}
+    for node_id, data in g.nodes(data=True):
+        if 'node' in data:
+            positions[node_id] = data['node'].pos
+    nx.draw_networkx(g, pos=positions, node_size=10, with_labels=False)
     plt.show()
     
+#def plot(g: CityGraph, filename: str) -> None:
+   # desa g com una imatge amb el mapa de la cuitat de fons en l'arxiu filename
+    ...
+#def plot_path(g: CityGraph, p: Path, filename: str) -> None: 
+    # mostra el camí p en l'arxiu filename
+    ...
+def main() -> None:
     
     
+    g1 = get_osmnx_graph()
+    g2 = get_buses_graph()
+    city = build_city_graph(g1, g2)
+    """
+    g1 = get_osmnx_graph()
+    g2 = get_buses_graph()
+    city = build_city_graph(g1, g2)
     
-def plot(g: nx.Graph, nom_fitxer: str) -> None:
- # Crear un objecte StaticMap amb el mapa de fons de la ciutat
-    city_map = staticmap.StaticMap(1000, 1000, url_template='https://a.tile.openstreetmap.org/{lat}/{lon}.png'.format(lat=41.3888, lon=2.159))
-
-    # Afegir les parades com a marcadors al mapa
-    for node_attrs in g.nodes.values():        
-        parada = node_attrs['node']        
-        city_map.add_marker(staticmap.CircleMarker(parada.pos, "red", 10))
+    """
+    show(city)
     
-    
-    for edge_attrs in g.edges.values():        
-        linia = edge_attrs['aresta']        
-        line = staticmap.Line((linia.node_origen.pos, linia.node_desti.pos), "blue", 5)
-        city_map.add_line(line)
-    
-    # Generar el mapa amb les parades i trajectes i desar el mapa com una imatge
-    city_map.render().save(nom_fitxer)
-
-   
-def main()-> None:
-    g = get_buses_graph()
-    show(g)
-    image_path = os.path.abspath('map.png')
-    plot(g,"map.png")
-    
-if __name__=='__main__':
+if __name__ ==  '__main__':
     main()
