285f71634e49c23286fc4e58627d7fddddace43a
aleix-albaiges <118050410+aleix-albaiges@users.noreply.github.com>
2023-05-30 19:24:17 +0200

    Update city.py

--- city.py
+++ city.py
@@ -1,65 +1,104 @@
 import networkx as nx
-from typing import TypeAlias
+from typing import TypeAlias, Optional
 import osmnx as ox
-from buses import *
 import pickle
 import matplotlib.pyplot as plt
-
+import os
+from dataclasses import dataclass
+from provabuses import *
 CityGraph : TypeAlias = nx.Graph
 OsmnxGraph : TypeAlias = nx.MultiDiGraph
-Coord : TypeAlias = tuple[float, float]   # (latitude, longitude)
+
+@dataclass
+class Cruilla:
+    id: int
+    pos: Coord
+    def __init__(self, id: int, pos:Coord) -> None:
+        self.id = id
+        self.pos = pos
+class Carrer:
+    node_origen_id: int
+    node_desti_id: int
+    longitud: float
+    info: str
+    def __init__(self, node_origen_id: int, node_desti_id: int, longitud: float, info:str) -> None:
+        self.node_origen_id = node_origen_id
+        self.node_desti_id = node_desti_id
+        self.longitud = longitud
+        self.info = info
+
 def get_osmnx_graph() -> OsmnxGraph:
-    ciutat = "Barcelona, Spain"
-    graf = ox.graph_from_place(ciutat, network_type="all")
-    multi_graf = nx.MultiDiGraph(graf)
-    return multi_graf
+    filename = 'graf_barcelona'
+    if os.path.exists(filename):
+        return load_osmnx_graph(filename)
+    else:
+        ciutat = "Barcelona, Spain"
+        graf = ox.graph_from_place(ciutat, network_type='walk', simplify=True)
+        multi_graf = nx.MultiDiGraph(graf)
+        save_osmnx_graph(graf,"graf_barcelona")
+        return multi_graf
 
 def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None:
     # guarda el graf g al fitxer filename
-    output_folder = "Desktop/PRACTICA2/" + filename
+    # Obtenir el directori actual
+    current_dir = os.getcwd()
+
+    # Definir la ruta del fitxer de sortida
+    output_folder = os.path.join(current_dir, filename)
     with open(output_folder, 'wb') as file:
         pickle.dump(g, file)
 
     
 def load_osmnx_graph(filename: str) -> OsmnxGraph: 
     # retorna el graf guardat al fitxer filename
-    ruta_document = 'Desktop/PRACTICA2/' + filename
+      # Obtenir el directori actual
+    current_dir = os.getcwd()
+
+    # Definir la ruta del fitxer
+    ruta_document = os.path.join(current_dir, filename)
+
+    # Comprovar si el fitxer existeix
+    if not os.path.exists(ruta_document):
+        raise FileNotFoundError(f"El fitxer '{filename}' no existeix en el directori actual.")
+
+    # Llegir el graf des del fitxer
     with open(ruta_document, 'rb') as file:
         graf = pickle.load(file)
     return graf
 
 def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph:
-     # retorna un graf fusió de g1 i g2
+    # retorna un graf fusió de g1 i g2
+    
     city_graph = nx.Graph()
-    for u, nbrsdict in g1.adjacency():      
-        city_graph.add_node(u, info = 'Cruilla')
-        # for each adjacent node v and its (u, v) edges' information ...
-        for v, edgesdict in nbrsdict.items():
-            city_graph.add_node(v, info = 'Cruilla')
-            eattr = edgesdict[0]  
-            city_graph.add_edge(u,v, **eattr,info = 'Carrer')
-            # osmnx graphs are multigraphs, but we will just consider their first edge
-             # eattr contains the attributes of the first edge
-            # we remove geometry information from eattr because we don't need it and takes a lot of space        
-    for node_attrs in g2.nodes.values():
-        parada = Parada(node_attrs['parada'].id,node_attrs['parada'].pos)
-        city_graph.add_node(parada.id,parada, info = Bus)
-    for edge_attrs in g2.edges.values():        
-        linia = edge_attrs['linia']  
-        city_graph.add_edge(linia.node_origen.pos, linia.node_desti.pos,info = 'Bus')
+    for node_id, data in g1.nodes(data=True):
+        cruilla = Cruilla(id=node_id, pos=(data['y'],data['x']))
+        city_graph.add_node(cruilla.id, pos= cruilla.pos, node = cruilla)
+    
+    for u, v, data in g1.edges(data=True):
+        carrer = Carrer(node_origen_id = u, node_desti_id = v, longitud= data['length'], info = 'Carrer')
+        if 'geometry' in data:
+            del(data['geometry'])
+        city_graph.add_edge(carrer.node_origen_id,carrer.node_desti_id, aresta = carrer)
     
     for node_attrs in g2.nodes.values():
-        parada = Parada(node_attrs['parada'].id,node_attrs['parada'].pos)
-        cruilla_propera = ox.get_nearest_node(g1, parada.pos)
-        city_graph.add_edge(parada, cruilla_propera, info =' Carrer')
+        parada = Parada(id = node_attrs['node'].id, pos = node_attrs['node'].pos)
+        city_graph.add_node(parada.id, pos = parada.pos, node = parada, info = 'Bus')
+        cruilla_propera = ox.nearest_nodes(g1, parada.pos[0], parada.pos[1])
+        city_graph.add_edge(parada.id,cruilla_propera, aresta = cruilla_propera, info = 'Carrer')
+        #no detecta que parada i cruilla_propera estiguin ja al graf i genera problems 
+        #perquè els torna a afegir sense atributs.
+    
+    
+    for edge_attrs in g2.edges.values():        
+        linia = edge_attrs['aresta']  
+        city_graph.add_edge(linia.node_origen.id, linia.node_desti.id, info = 'Bus')
+    
+        
         
     return city_graph
-    
-    
-    
-   
 
-def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path:
+"""
+def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path: 
     src_node = ox.distance.nearest_nodes(ox_g, src[0], src[1])
     dst_node = ox.distance.nearest_nodes(ox_g, dst[0], dst[1])
 
@@ -67,27 +106,35 @@ def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path:
     shortest_edges = [(shortest_path[i], shortest_path[i+1]) for i in range(len(shortest_path)-1)]
     
     return Path(shortest_path, shortest_edges)
-
+"""
 def show(g: CityGraph) -> None:
     # mostra g de forma interactiva en una finestra
-    posicions = nx.get_node_attributes(g, 'pos')
-    nx.draw(g, pos=posicions, with_labels=False,font_size=5,node_size= 5)
+    positions = {}
+    for node_id, data in g.nodes(data=True):
+        if 'node' in data:
+            positions[node_id] = data['node'].pos
+    nx.draw_networkx(g, pos=positions, node_size=10, with_labels=False)
     plt.show()
-
-    #nx.draw(g)
     
-def plot(g: CityGraph, filename: str) -> None: ...
+#def plot(g: CityGraph, filename: str) -> None:
    # desa g com una imatge amb el mapa de la cuitat de fons en l'arxiu filename
-
-def plot_path(g: CityGraph, p: Path, filename: str, ...) -> None: ...
+    ...
+#def plot_path(g: CityGraph, p: Path, filename: str) -> None: 
     # mostra el camí p en l'arxiu filename
-    
+    ...
 def main() -> None:
-    save_osmnx_graph(get_osmnx_graph(), 'graf_barcelona')
-    g1 = load_osmnx_graph('graf_barcelona')
+    
+    
+    g1 = get_osmnx_graph()
+    g2 = get_buses_graph()
+    city = build_city_graph(g1, g2)
+    """
+    g1 = get_osmnx_graph()
     g2 = get_buses_graph()
     city = build_city_graph(g1, g2)
     
+    """
     show(city)
-if __name__ == '__main__':
+    
+if __name__ ==  '__main__':
     main()
