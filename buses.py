621e6f193be5d1cdc85ed91bf53adea9d367fbaf
aleix-albaiges <118050410+aleix-albaiges@users.noreply.github.com>
2023-06-01 17:36:49 +0200

    Update buses.py

--- buses.py
+++ buses.py
@@ -24,12 +24,12 @@ def get_buses_graph() -> nx.Graph:
         parades = data['ObtenirDadesAMBResult']['Linies']['Linia'][i]['Parades']['Parada']
         for parada in parades:
             if parada['Municipi'] == 'Barcelona':
-                node_id = parada['CodAMB']
-                node_x, node_y = parada['UTM_X'], parada['UTM_Y']
+                node_id = int(parada['CodAMB'])
+                node_lon, node_lat = parada['UTM_Y'], parada['UTM_X']
 
                 # Afegir el node amb els atributs de nom i coordenades
 
-                graph.add_node(node_id, pos=(node_x,node_y))
+                graph.add_node(node_id, pos=(node_lon,node_lat))
 
     # Processar les arestes i afegir-les com a arestes amb els seus atributs corresponents
     for linia in data['ObtenirDadesAMBResult']['Linies']['Linia']:
@@ -41,12 +41,12 @@ def get_buses_graph() -> nx.Graph:
             dic_node_origen = parades_linia[i]
             dic_node_desti = parades_linia[i+1]
             if dic_node_origen['Municipi'] == 'Barcelona' and dic_node_desti['Municipi'] == 'Barcelona':               
-                node_origen_id = dic_node_origen['CodAMB']
-                node_desti_id = dic_node_desti['CodAMB']
+                node_origen_id = int(dic_node_origen['CodAMB'])
+                node_desti_id = int(dic_node_desti['CodAMB'])
                 
             # Afegir l'aresta amb l'atribut de la línia de bus
             if node_origen_id != node_desti_id:
-                graph.add_edge(node_origen_id, node_desti_id, )
+                graph.add_edge(node_origen_id, node_desti_id)
 
     return graph
 
@@ -61,20 +61,19 @@ def show(g: BusesGraph) -> None:
     
 def plot(g: nx.Graph, nom_fitxer: str) -> None:
 # Crear un objecte StaticMap amb el mapa de fons de la ciutat
-    city_map = staticmap.StaticMap(800, 800)
-    city_map.add_image_layer(staticmap.ImageLayer(nom_fitxer))
+    city_map = staticmap.StaticMap(3500, 3500)
 
     # Afegir les parades com a marcadors al mapa
     for node in g.nodes:
         x, y = g.nodes[node]['pos']
-        city_map.add_marker(staticmap.CircleMarker((x,y), "red", 10))
+        city_map.add_marker(staticmap.CircleMarker((x,y), "red", 6))
 
     # Afegir els trajectes com a línies al mapa
     for edge in g.edges:
         node1, node2 = edge
         x1, y1 = g.nodes[node1]['pos']
         x2, y2 = g.nodes[node2]['pos']
-        line = staticmap.Line(((x1,y1),(x2,y2)), "blue",5)
+        line = staticmap.Line(((x1,y1),(x2,y2)), "blue", 2)
 
         city_map.add_line(line)
     # Generar el mapa amb les parades i trajectes
