3e5b05507a14fc554dc4171e3815f1f74f3d812b
aleix-albaiges <118050410+aleix-albaiges@users.noreply.github.com>
2023-06-04 13:49:04 +0200

    Update buses.py

--- buses.py
+++ buses.py
@@ -63,7 +63,7 @@ def show(g: BusesGraph) -> None:
     plt.show()
 
 
-def plot(g: BusesGraph, filename: str) -> None:
+def plot_buses(g: BusesGraph, filename: str) -> None:
     """
     Desa g com una imatge amb el mapa de la
     ciutat de fons en l'arxiu filename.
@@ -74,23 +74,16 @@ def plot(g: BusesGraph, filename: str) -> None:
     # Afegir les parades com a marcadors al mapa
     for node in g.nodes:
         x, y = g.nodes[node]['pos']
-        if g.nodes[node]['tipus'] == "Cruilla":
-            map.add_marker(
-                staticmap.CircleMarker((x, y), "black", 15))
-        else:
-            map.add_marker(staticmap.CircleMarker((x, y), "blue", 15))
+        map.add_marker(
+            staticmap.CircleMarker((x, y), "black", 15))
 
     # Afegir els trajectes com a línies al mapa
     for u, v, data in g.edges(data=True):
         x1, y1 = g.nodes[u]['pos']
         x2, y2 = g.nodes[v]['pos']
-
-        if data['tipus'] == "Carrer":
-            line = staticmap.Line(((x1, y1), (x2, y2)), "red", 15)
-        else:
-            line = staticmap.Line(((x1, y1), (x2, y2)), "green", 15)
-
+        line = staticmap.Line(((x1, y1), (x2, y2)), "red", 15)
         map.add_line(line)
+
     # Generar el mapa amb les parades i trajectes
     image = map.render()
 
